6ff615c783b336f4d19c994213072c948b5ac62d
CooperHoag <cooperhoag@gmail.com>
2025-07-08 12:16:14 -0700

    Add users, movies, user_ratings tables in schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,28 @@
+DROP TABLE IF EXISTS user_ratings;
+DROP TABLE IF EXISTS movies;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
   username text NOT NULL UNIQUE,
   password text NOT NULL
+  bio TEXT
+);
+
+CREATE TABLE movies (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  genre VARCHAR(50) NOT NULL,
+  runtime INT, 
+  release_year DATE,
+  director VARCHAR(100),
+  movie_poster TEXT,
+  plot_summary TEXT
+);
+
+CREATE TABLE user_ratings (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  movie_id INT REFERENCES movies(id) ON DELETE CASCADE,
+  rating INT CHECK (rating >= 1 AND rating <= 5)
 );
