1a4820e862a950d56478fc054cbeaa3b86daf52f
Yulonda Joy Wyatt <yulondawyatt@gmail.com>
2025-07-10 13:19:21 -0400

    Added subgenre_one & subgenre_two to queries & fixed additional bugs found

--- db/queries/movies.js
+++ db/queries/movies.js
@@ -2,14 +2,14 @@ import db from '#db/client';
 
 
 /** @return the movie created with the provided details */ 
-export async function createMovie (title, genre, runtime, releaseDate, director, moviePoster, plotSummary) {
+export async function createMovie (title, genre, subgenreOne, subgenreTwo, runtime, releaseDate, director, moviePoster, plotSummary) {
   const sql = `
-    INSERT INTO movies (title, genre, runtime, release_date, director, movie_poster, plot_summary)
-    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    INSERT INTO movies (title, genre, subgenre_one, subgenre_two, runtime, release_date, director, movie_poster, plot_summary)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
     RETURNING *
   `;
 
-  const { rows: [newMovie] } = await db.query(sql, [title, genre, runtime, releaseDate, director, moviePoster, plotSummary]);
+  const { rows: [newMovie] } = await db.query(sql, [title, genre, subgenreOne, subgenreTwo, runtime, releaseDate, director, moviePoster, plotSummary]);
   console.log(newMovie);
   return newMovie;
 };
@@ -17,8 +17,7 @@ export async function createMovie (title, genre, runtime, releaseDate, director,
 
 /** @return all movies */
 export async function getAllMovies () {
-  const sql = `SELECT * FROM movies
-  RETURNING *`;
+  const sql = `SELECT * FROM movies`;
 
   const { rows: moviesList } = await db.query(sql);
   console.log(moviesList);
@@ -29,25 +28,24 @@ export async function getAllMovies () {
 /** @returns the movie with the given id */
 /** @returns UNDEFINED if a movie with the given ID does not exist */
 export async function getMovieById (id) {
-  const sql = `SELECT * FROM movies WHERE id=$1
-  RETURNING *`;
+  const sql = `SELECT * FROM movies WHERE id=$1`;
 
   const { rows: [singleMovieById] } = await db.query(sql, [id]);
-  console.log(singleMovieById[0]);
-  return singleMovieById[0] || undefined;
+  console.log(singleMovieById);
+  return singleMovieById || undefined;
 };
 
 
 /** @returns the updated movie with the given ID */
 /** @return UNDEFINED if movie with the given ID does not exist */
-export async function updateMovieById (title, genre, runtime, releaseDate, director, moviePoster, plotSummary) {
-const sql = `UPDATE movies SET title=$2, genre=$3, runtime=$4, release_date=$5, director=$6, movie_poster=$7, plot_summary=$8
+export async function updateMovieById (id, title, genre, subgenreOne, subgenreTwo, runtime, releaseDate, director, moviePoster, plotSummary) {
+const sql = `UPDATE movies SET title=$2, genre=$3, subgenre_one=$4, subgenre_two=$5, runtime=$6, release_date=$7, director=$8, movie_poster=$9, plot_summary=$10
 WHERE id=$1
 RETURNING *`;
 
-const { rows: [updatedMovie] } = db.query(sql, [title, genre, runtime, releaseDate, director, moviePoster, plotSummary]);
-console.log(updatedMovie[0]);
-return updatedMovie[0] || undefined;
+const { rows: [updatedMovie] } = await db.query(sql, [id, title, genre, subgenreOne, subgenreTwo, runtime, releaseDate, director, moviePoster, plotSummary]);
+console.log(updatedMovie);
+return updatedMovie || undefined;
 };
 
 
@@ -57,9 +55,9 @@ export async function deleteMovieById (id) {
 const sql =`DELETE FROM movies WHERE id=$1
 RETURNING *`
 
-const { rows: [deletedMovie] } = db.query(sql, [id]);
-console.log(deletedMovie[0]);
-return deletedMovie[0];
+const { rows: [deletedMovie] } = await db.query(sql, [id]);
+console.log(deletedMovie);
+return deletedMovie;
 };
 
 
