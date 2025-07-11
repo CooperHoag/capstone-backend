3e75d32bf50eabc75e9801a4f3d4b7cfefa3c27e
CooperHoag <cooperhoag@gmail.com>
2025-07-11 07:47:48 -0700

    Add createMovie function in db/queries/movies.js

--- db/queries/movies.js
+++ db/queries/movies.js
@@ -14,4 +14,15 @@ export async function getMovieById(id) {
   return singleMovieById || undefined;
 }
 
-
+export async function createMovie (title, genres, runtime, releaseDate, director, moviePoster, plotSummary) {
+  const sql = `
+    INSERT INTO movies (title, genres, runtime, release_date, director, movie_poster, plot_summary)
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    RETURNING *
+  `;
+ 
+ 
+  const { rows: [newMovie] } = await db.query(sql, [title, genres, runtime, releaseDate, director, moviePoster, plotSummary]);
+  console.log(newMovie);
+  return newMovie;
+};
