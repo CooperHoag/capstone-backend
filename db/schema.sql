c6c27b8e27ef8db47c75ae3485cd3e7cffee7455
Dylan Jules <dylan.jules@oakwood.edu>
2025-07-15 17:19:16 -0400

    The schema had some issues with order

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,12 @@
-DROP TABLE IF EXISTS reviews CASCADE;
-DROP TABLE IF EXISTS watchlist CASCADE;
+DROP TABLE IF EXISTS user_genre_selections CASCADE;
+DROP TABLE IF EXISTS genres CASCADE;
 DROP TABLE IF EXISTS user_ratings CASCADE;
+DROP TABLE IF EXISTS watchlist CASCADE;
+DROP TABLE IF EXISTS reviews CASCADE;
 DROP TABLE IF EXISTS movies CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS recommended_page CASCADE;
+
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
@@ -14,6 +17,16 @@ CREATE TABLE users (
   password text NOT NULL,
   bio TEXT,
   profile_image_url TEXT
+);
+CREATE TABLE recommended_page (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  genres TEXT NOT NULL,
+  runtime INT, 
+  release_date DATE,
+  director VARCHAR(50),
+  movie_poster TEXT,
+  plot_summary TEXT
 );
 
 CREATE TABLE movies (
@@ -26,6 +39,13 @@ CREATE TABLE movies (
   movie_poster TEXT,
   plot_summary TEXT
 );
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  movie_id INT REFERENCES movies(id) ON DELETE CASCADE,
+  content TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 CREATE TABLE user_ratings (
   id SERIAL PRIMARY KEY,
@@ -34,16 +54,6 @@ CREATE TABLE user_ratings (
   rating BOOLEAN
 );
 
-CREATE TABLE recommended_page (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(50) NOT NULL,
-  genres TEXT NOT NULL,
-  runtime INT, 
-  release_date DATE,
-  director VARCHAR(50),
-  movie_poster TEXT,
-  plot_summary TEXT
-);
 
 CREATE TABLE watchlist (
   id SERIAL PRIMARY KEY,
@@ -63,13 +73,5 @@ CREATE TABLE user_genre_selections (
   PRIMARY KEY (user_id, genre_id)
 );
 
-INSERT INTO genres (genre_label) VALUES
-('Action'), ('Horror'), ('Romance'), ('Sci-Fi'), ('Drama'), ('Comedy');
 
-CREATE TABLE reviews (
-  id SERIAL PRIMARY KEY,
-  user_id INT REFERENCES users(id) ON DELETE CASCADE,
-  movie_id INT REFERENCES movies(id) ON DELETE CASCADE,
-  content TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+
